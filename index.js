e63f14660bebedbe63db1e7008d960ce3f03d103
Vongola <1085280247@qq.com>
2019-08-13 09:12:10 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa')
+Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
+const db = require('./db')
+const cache = require('./cache')
+
+router.get('/cache', async(ctx, next) => {
+  let top5 =  await cache.get('top5')
+  console.log('get chche:', top5 && top5.length)
+  if (!top5) {
+    top5 = await db.query('select * from todos limit 5')
+    try {
+      await cache.set('top5', JSON.stringify(top5), 'PX', 10)
+    } catch (error) {
+      ctx.body = []
+    }
+    console.log('set chche:', top5.length)
+  }
+  ctx.body = top5
+})
+
+app
+  .use(router.routes())
+  .listen(4000)
