38d3aa3567807dc057d1709cd60968c9f17e80d4
Vongola <1085280247@qq.com>
2019-08-13 14:15:34 +0800

    完成redis同步锁

--- cache.js
+++ cache.js
@@ -8,12 +8,67 @@ const client = redis.createClient({
   port: config.port
 })
 
+const sleep = (time = 0) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  })
+}
+const tryLock = async (key, timeout, resolve, reject, self) => {
+  // 超时获取新锁,删除原来的锁, 没有超时就进入抢锁
+  const currentLockTime = await self.get(key)
+  if (currentLockTime < Date.now()) {
+    client.getset(key, timeout + Date.now(), async (err, oldTime) => {
+      if (err) return reject(err)
+      // 两个锁时间相同才证明是同一个锁，否则已经被抢，进入抢锁阶段
+      if (currentLockTime == oldTime && oldTime !== null) {
+        client.setnx(key, timeout + Date.now(), async (err, islock) => {
+          if (err) reject(err)
+          else {
+            await self.unlock(key)
+            client.setnx(key, timeout + Date.now(), (err, islock) => {
+              if (err) reject(err)
+              else resolve(islock)
+            })
+          }
+        })
+      } else {
+        // 没有取到锁，一段时间后重新获取
+        while(true) {
+          if (currentLockTime < Date.now()) {
+            tryLock(key, timeout, resolve, reject, self)
+            break
+          } else {
+            await sleep(timeout)
+          }
+        }
+      }
+    })
+  } else {
+    while(true) {
+      if (currentLockTime < Date.now()) {
+        tryLock(key, timeout, resolve, reject, self)
+        break
+      } else {
+        await sleep(timeout)
+      }
+    }
+  }
+}
+
 module.exports = {
   get: async (key) => {
     return new Promise((resolve, reject) => {
       client.get(key, (err, data) => {
         if (err) reject(err)
-        else resolve(data)
+        else {
+          try {
+            resolve(data ? JSON.parse(data) : data)
+          } catch {
+            resolve('')
+          }
+        }
       })
     })
   },
@@ -24,5 +79,31 @@ module.exports = {
         else resolve(data)
       })
     })
-  }
+  },
+  lock: async function (key, timeout) {
+    /// 考虑两个因素：
+    /// 1. 某个请求获取到锁后，在解锁时，redis出错，可能造成死锁，所以要设置超时时间。
+    /// 2. 设置了超时时间会有一个新的问题，如果多个请求获取到超时，那么会多次删除锁，但是之前获得锁的请求依然会执行先去。
+    /// 例如： b,c 读取到超时后，删除锁，设置新锁，b获得锁，然后c也删除锁，设置新锁，c也获得锁，所以b，c同时有锁。
+    const self = this
+    return new Promise((resolve, reject) => {
+      client.setnx(key, timeout + Date.now(), (err, islock) => {
+        if (err) return reject(err)
+        if (islock) {
+          resolve(islock)
+        } else {
+          // 没有获得锁就判断是否超时
+          tryLock(key, timeout, resolve, reject, self)
+        }
+      })
+    })
+  },
+  unlock: async (key) => {
+    return new Promise((resolve, reject) => {
+      client.del(key, (err, data) => {
+        if (err) reject(err)
+        else resolve(data)
+      })
+    })
+  },
 }
--- index.js
+++ index.js
@@ -11,13 +11,20 @@ router.get('/cache', async(ctx, next) => {
   let top5 =  await cache.get('top5')
   console.log('get chche:', top5 && top5.length)
   if (!top5) {
-    top5 = await db.query('select * from todos limit 5')
     try {
-      await cache.set('top5', JSON.stringify(top5), 'PX', 10)
+      if (await cache.lock('lockTop', 20)) {
+        top5 = await db.query('select * from todos limit 5')
+        await cache.set('top5', JSON.stringify(top5), 'PX', 1)
+        console.log('set chche:', top5.length)
+        await cache.unlock('lockTop')
+      } else {
+        // 测试是否没锁
+        console.log('没锁')
+      }
     } catch (error) {
+      console.log(error)
       ctx.body = []
     }
-    console.log('set chche:', top5.length)
   }
   ctx.body = top5
 })
