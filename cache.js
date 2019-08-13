3e5c83e3ad747d6e302ef52eab93281d9a61639b
dirk <inspiration520@gmail.com>
2019-08-13 23:34:10 +0800

    project init

---
+++ cache.js
@@ -0,0 +1,26 @@
+const config = require("./config").redis;
+const redis = require("redis");
+
+const client = redis.createClient({
+  host: config.host,
+  port: config.port
+});
+
+module.exports = {
+  get: async key => {
+    return new Promise((resolve, reject) => {
+      client.get(key, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  },
+  set: async (key, value, exp, tim) => {
+    return new Promise((resolve, reject) => {
+      client.set(key, value, exp, tim, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  }
+};
---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "Ropu0877",
+    database: "cache",
+    port: 3306
+  },
+  redis: {
+    host: "localhost",
+    port: 6379
+  }
+};
